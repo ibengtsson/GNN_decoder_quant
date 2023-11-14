2b96e79ebf666abb857296a39b4b488714532de6
Isak Bengtsson <isakb97@gmail.com>
2023-11-14 16:22:21 +0100

    runnable

--- test.py
+++ test.py
@@ -1,27 +1,34 @@
 import numpy as np
+from tqdm import tqdm
+import torch
 import torch.nn as nn
+from torch_geometric.data import Data
+from torch_geometric.loader import DataLoader
 from src.GNN_Decoder import GNN_Decoder
 from src.gnn_models import GNN_7, GNN_7_DenseConv
-
+from src.simulations import SurfaceCodeSim
+from src.graph_representation import get_3D_graph
 
 if __name__ == "__main__":
-    
     # code and noise settings
-    code_sz = 7
-    p = 1e-3
+    code_sz = 5
+    p = 3e-3
     reps = 10
-    
+
     # training settings
     n_epochs = 1
+    n_graphs = 5000
     batch_sz = 4
     lr = 1e-3
     loss = nn.BCEWithLogitsLoss()
-    
+    seed = 11
+
     # graph settings
     n_node_feats = 5
     power = 1
-    cuda = False
-    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    cuda = True if torch.cuda.is_available() else False
+
     # initialise decoder parameters
     gnn_params = {
         "model": {
@@ -30,6 +37,7 @@ if __name__ == "__main__":
             "loss": loss,
             "num_node_features": n_node_feats,
             "initial_learning_rate": lr,
+            "manual_seed": seed,
         },
         "graph": {
             "num_node_features": n_node_feats,
@@ -39,7 +47,39 @@ if __name__ == "__main__":
     }
     
     decoder = GNN_Decoder(gnn_params)
-    print("Decoder structure was successfully created")
+    print("Decoder was successfully created")
     
+    sim = SurfaceCodeSim(reps, code_sz, p, n_shots=5000, seed=seed)
+    syndromes, flips = sim.generate_syndromes(n_graphs)
+
+    graphs = []
+    for syndrome, flip in zip(syndromes, flips):
+        graph = get_3D_graph(
+            syndrome_3D=syndrome,
+            target=flip,
+            power=power,
+        )
+
+        graphs.append(
+            Data(
+                x=torch.from_numpy(graph[0]).to(device),
+                edge_index=torch.from_numpy(graph[1]).to(device),
+                edge_attr=torch.from_numpy(graph[2]).to(device),
+                y=torch.from_numpy(graph[3]).to(device),
+            )
+        )
+
+    loader = DataLoader(graphs, batch_size=128)
+
+    print(f"We have #{len(loader)} batches.")
+    # run forward pass
+    for batch in tqdm(loader):
+        batch.batch = batch.batch.to(device)
+        out = decoder(
+            x=batch.x,
+            edge_index=batch.edge_index,
+            edge_attr=batch.edge_attr,
+            batch=batch.batch,
+        )
     
-    
+    print(f"Mean value of output: {out.mean()}")
