8cff59463460e86c81ce64c6654f3890f7ba90d4
Isak Bengtsson <isakb97@gmail.com>
2023-11-16 10:24:33 +0100

    fixed bug where batch size does not change

--- test.py
+++ test.py
@@ -13,8 +13,8 @@ from src.graph_representation import get_3D_graph
 # profiling
 from torch.profiler import profile, record_function, ProfilerActivity
 
+
 def main():
-    
     # default settings:
     # code and noise settings
     code_sz = 5
@@ -32,13 +32,15 @@ def main():
     # graph settings
     n_node_feats = 5
     power = 1
-    
+
     # read input arguments and potentially overwrite default settings
-    parser = argparse.ArgumentParser(description="Choose model and optionally GPU instance.")
+    parser = argparse.ArgumentParser(
+        description="Choose model and optionally GPU instance."
+    )
     parser.add_argument("-m", "--model", required=True)
     parser.add_argument("-b", "--batch_size", required=False)
     parser.add_argument("-g", "--gpu", required=False)
-    
+
     args = parser.parse_args()
     if args.model == "GNN_7":
         model_class = GNN_7
@@ -47,18 +49,18 @@ def main():
     else:
         print("Using default model class, GNN_7.")
         model_class = GNN_7
-    
-    if args.gpu:   
+
+    if args.gpu:
         device = torch.device(args.gpu if torch.cuda.is_available() else "cpu")
         print(f"Using {device}.")
     else:
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         print(f"Using default device {device}.")
-    
+
     if args.batch_size:
         batch_size = args.batch_size
         print(f"Using batch size {batch_size}.")
-    else: 
+    else:
         print(f"Using default batch size {batch_size}.")
 
     # initialise decoder parameters
@@ -77,10 +79,10 @@ def main():
         },
         "device": device,
     }
-    
+
     decoder = GNN_Decoder(gnn_params)
     print("Decoder was successfully created")
-    
+
     sim = SurfaceCodeSim(reps, code_sz, p, n_shots=n_graphs, seed=seed)
     syndromes, flips = sim.generate_syndromes(n_graphs)
 
@@ -101,29 +103,30 @@ def main():
             )
         )
 
-    loader = DataLoader(graphs, batch_size=5012)
+    loader = DataLoader(graphs, batch_size=batch_size)
 
     print(f"We have #{len(loader)} batches.")
     # run forward pass
-    for batch in tqdm(loader):
-        
-        # move what we need to gpu
-        x = batch.x.to(device)
-        edge_index = batch.edge_index.to(device)
-        edge_attr = batch.edge_attr.to(device)
-        batch_label = batch.batch.to(device)
-        
-        with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True) as prof:
-            with record_function("forward pass"):
+    with profile(
+        activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True
+    ) as prof:
+        with record_function("forward pass"):
+            for batch in loader:
+                # move what we need to gpu
+                x = batch.x.to(device)
+                edge_index = batch.edge_index.to(device)
+                edge_attr = batch.edge_attr.to(device)
+                batch_label = batch.batch.to(device)
+
                 out = decoder(
                     x=x,
                     edge_index=edge_index,
                     edge_attr=edge_attr,
                     batch=batch_label,
                 )
-        break
-    
+
     print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
-    
+
+
 if __name__ == "__main__":
     main()
