aa139a8040a82806f99704db0ccc9c198fbf1aaa
Isak Bengtsson <isakb97@gmail.com>
2023-11-22 16:11:36 +0100

    removed unneccesary comments

--- src/gnn_models.py
+++ src/gnn_models.py
@@ -43,19 +43,6 @@ class GNN_7(torch.nn.Module):
         
         # Output later
         self.output_layer = Linear(hidden_channels_MLP[-1], num_classes)
-        
-        # self.graph1 = GraphConv(num_node_features, hidden_channels_GCN[0])
-        # self.graph2 = GraphConv(hidden_channels_GCN[0], hidden_channels_GCN[1])
-        # self.graph3 = GraphConv(hidden_channels_GCN[1], hidden_channels_GCN[2])
-        # self.graph4 = GraphConv(hidden_channels_GCN[2], hidden_channels_GCN[3])
-        # self.graph5 = GraphConv(hidden_channels_GCN[3], hidden_channels_GCN[4])
-        # self.graph6 = GraphConv(hidden_channels_GCN[4], hidden_channels_GCN[5])
-        # self.graph7 = GraphConv(hidden_channels_GCN[5], hidden_channels_GCN[6])
-        # # Layers for graph embedding classifier
-        # self.lin1 = Linear(hidden_channels_GCN[6], hidden_channels_MLP[0])
-        # self.lin2 = Linear(hidden_channels_MLP[0], hidden_channels_MLP[1])
-        # self.lin3 = Linear(hidden_channels_MLP[1], hidden_channels_MLP[2])
-        # self.lin4 = Linear(hidden_channels_MLP[2], num_classes)
 
     def forward(self, x, edge_index, edge_attr, batch, fix_pt=True):
         
@@ -79,38 +66,6 @@ class GNN_7(torch.nn.Module):
         
         return x
         
-        x = self.graph1(x, edge_index, edge_weight=edge_attr)
-        x = x.relu()
-        x = self.graph2(x, edge_index, edge_weight=edge_attr)
-        x = x.relu()
-        x = self.graph3(x, edge_index, edge_weight=edge_attr)
-        x = x.relu()
-        x = self.graph4(x, edge_index, edge_weight=edge_attr)
-        x = x.relu()
-        x = self.graph5(x, edge_index, edge_weight=edge_attr)
-        x = x.relu()
-        x = self.graph6(x, edge_index, edge_weight=edge_attr)
-        x = x.relu()
-        x = self.graph7(x, edge_index, edge_weight=edge_attr)
-        x = x.relu()
-
-        # obtain graph embedding
-        if fix_pt:
-            x = global_mean_pool(x, batch).char()
-        else:
-            x = global_mean_pool(x, batch)
-        # Apply X(Z) classifier
-        X = self.lin1(x)
-        X = X.relu()
-        X = self.lin2(X)
-        X = X.relu()
-        X = self.lin3(X)
-        X = X.relu()
-        X = self.lin4(X)
-
-        return X
-
-
 class GNN_7_DenseConv(torch.nn.Module):
     """
     GNN with 7 consecutive GraphConv layers, whose final output is converted
