d26d612a3c445b08df302b231b5a9d27f4509178
Isak Bengtsson <isakb97@gmail.com>
2023-11-23 10:43:32 +0100

    moved utility functions to seperate file

---
+++ src/utils.py
@@ -0,0 +1,122 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from collections import OrderedDict
+import matplotlib.pyplot as plt
+import re
+
+
+def match_and_load_state_dict(
+    model: nn.Module,
+    trained_state_dict: OrderedDict,
+) -> nn.Module:
+    new_dict = OrderedDict(trained_state_dict)
+    for new_key, key in zip(model.state_dict().keys(), trained_state_dict.keys()):
+        new_dict[new_key] = new_dict[key]
+        del new_dict[key]
+
+    model.load_state_dict(new_dict)
+    return model
+
+
+def plot_weights(
+    n_graph_layers: int,
+    n_dense_layers: int,
+    weights: OrderedDict[str, torch.Tensor],
+) -> (plt.figure, plt.figure):
+    graph_dict = {"lin_rel.weight": 0, "lin_rel.bias": 1, "lin_root.weight": 2}
+    dense_dict = {"weight": 0, "bias": 1}
+    graph_fig, graph_ax = plt.subplots(
+        n_graph_layers, 3, figsize=(4 * 3, 4 * n_graph_layers)
+    )
+    dense_fig, dense_ax = plt.subplots(
+        n_dense_layers, 2, figsize=(4 * 2, 4 * n_dense_layers)
+    )
+
+    for key, weight in weights.items():
+        index = int(re.findall(r"\d+", key)[0])
+        data = weight.numpy().flatten()
+        data_sz = data.shape[0]
+
+        if "graph" in key:
+            split_key = key.split(sep=".")
+            plot_key = split_key[-2] + "." + split_key[-1]
+            ax = graph_ax[index - 1][graph_dict[plot_key]]
+            ax.hist(data, 50)
+            ax.set_title(f"GraphConv {index}: {plot_key} ({data_sz} parameters)")
+
+        else:
+            split_key = key.split(sep=".")
+            plot_key = split_key[-1]
+            ax = dense_ax[index - 1][dense_dict[plot_key]]
+            ax.hist(weight.numpy().flatten(), 50)
+            ax.set_title(f"Linear {index}: {plot_key} ({data_sz} parameters)")
+
+        ax.set_xlabel(
+            f"$\mu$ = {data.mean():.2f}, $\sigma^2$ = {data.std():.2f}, $x \in$ [{data.min():.2f}, {data.max():.2f}]"
+        )
+
+    graph_fig.tight_layout()
+    dense_fig.tight_layout()
+
+    return graph_fig, dense_fig
+
+def get_number_of_model_layers(module: nn.Module):
+    
+    sum = 0
+    for child in module.children():
+        if isinstance(child, nn.modules.container.ModuleList):
+            n = get_number_of_model_layers(child)
+            sum += n
+        else:
+            sum += 1
+    return sum
+
+def quantize_tensor(
+    r: torch.Tensor,
+    scale: torch.Tensor | float,
+    zero_point: torch.Tensor | int,
+    bit_width: int = 8,
+    signed: bool = True,
+) -> torch.Tensor:
+    if signed:
+
+        return torch.clamp(
+            torch.round(r / scale + zero_point),
+            -(2 ** (bit_width - 1)),
+            2 ** (bit_width - 1) - 1,
+        )
+    else:
+        return torch.clamp(torch.round(r / scale + zero_point), 0, 2**bit_width - 1)
+
+
+
+def dequantize_tensor(
+    q: torch.Tensor,
+    scale: torch.Tensor | float,
+    zero_point: torch.Tensor | int,
+) -> torch.Tensor:
+
+    return (scale * (q - zero_point)).float()
+
+
+
+def get_scale(
+    alpha: torch.Tensor | float,
+    beta: torch.Tensor | float,
+    bit_width: int = 8,
+) -> torch.Tensor | float:
+
+    return (beta - alpha) / (2**bit_width - 1)
+
+
+
+def get_zero_pt(
+    alpha: torch.Tensor | float,
+    beta: torch.Tensor | float,
+    bit_width: int = 8,
+) -> torch.Tensor | int:
+
+    return torch.round(
+        (2**bit_width * (alpha + beta) - 2 * beta) / (2 * (alpha - beta))
+    )
