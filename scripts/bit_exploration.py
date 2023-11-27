b4a97149fc051f05d5b1ae9a0a769d48f4573668
Isak Bengtsson <isakb97@gmail.com>
2023-11-27 16:19:09 +0100

    more graphs

--- scripts/bit_exploration.py
+++ scripts/bit_exploration.py
@@ -391,8 +391,8 @@ def main():
     ]
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    n_graphs = int(1e3)
-    n_graphs_per_sim = int(1e3)
+    n_graphs = int(5e4)
+    n_graphs_per_sim = int(1e4)
     batch_size = 80000 if "cuda" in device.type else 4000
     p = 1e-3
     min_bits = 6
