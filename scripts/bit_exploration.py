19664e3c1c85fd66041894ed273d00c79252b8e9
Isak Bengtsson <isakb97@gmail.com>
2023-11-27 17:00:11 +0100

    changed max number of graphs

--- scripts/bit_exploration.py
+++ scripts/bit_exploration.py
@@ -392,7 +392,7 @@ def main():
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     n_graphs = int(1e7)
-    n_graphs_per_sim = int(5e4)
+    n_graphs_per_sim = int(5e5)
     batch_size = 80000 if "cuda" in device.type else 4000
     p = 1e-3
     min_bits = 2
