1b0973b8e7460565428ae3e4cd90123e1ba326b1
Isak Bengtsson <isakb97@gmail.com>
2023-11-27 16:17:18 +0100

    changed file structure

---
+++ bash_scripts/bit_exploration.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#SBATCH -A NAISS2023-5-353 -p alvis
+#SBATCH -t 0-03:00:00 			# time limit days-hours:minutes:seconds
+#SBATCH -J bit_exploration
+#SBATCH -o ./job_outputs/bit_exploration%j.out
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus-per-node=A40:1 # GPUs 64GB of RAM; cost factor 1.0
+
+# load modules and environment
+module purge
+source ~/scripts/load_env.sh
+
+# send script
+python3 ../scripts/bit_exploration.py
