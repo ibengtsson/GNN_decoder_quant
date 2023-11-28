924159c3a96af3fbad178a57dcbd94e9ed0dcdd3
Isak Bengtsson <isakb97@gmail.com>
2023-11-28 13:54:56 +0100

    fixed naming

--- bash_scripts/cluster_inference.sh
+++ bash_scripts/cluster_inference.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH -A NAISS2023-5-353 -p alvis
 #SBATCH -t 0-03:00:00 			# time limit days-hours:minutes:seconds
-#SBATCH -J d7_d_t_5
+#SBATCH -J d5_d_t_5
 #SBATCH -o ../job_outputs/d5_d_t_5_id%j.out
 #SBATCH --cpus-per-task=1
 #SBATCH --gpus-per-node=A40:1 # GPUs 64GB of RAM; cost factor 1.0
