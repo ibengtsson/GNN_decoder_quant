292e213f87c21aaf44d8c3f73ead4dbca2132249
Isak Bengtsson <isakb97@gmail.com>
2023-12-02 17:54:17 +0100

    add option to run quantization per layer

--- bash_scripts/bit_exploration.sh
+++ bash_scripts/bit_exploration.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #SBATCH -A NAISS2023-5-353 -p alvis
-#SBATCH -t 0-03:00:00 			# time limit days-hours:minutes:seconds
+#SBATCH -t 2-00:00:00 			# time limit days-hours:minutes:seconds
 #SBATCH -J bit_exploration
 #SBATCH -o ../job_outputs/bit_exploration%j.out
 #SBATCH --cpus-per-task=1
@@ -11,5 +11,6 @@ module purge
 source ~/scripts/load_env.sh
 
 # send script
-# python3 ../scripts/bit_exploration.py -e "weights_per_layer" -n 10000000 -ns 100000
-python3 ../scripts/bit_exploration.py -e "data" -n 10000000 -ns 100000
+python3 ../scripts/bit_exploration.py -e "weights_per_layer" -n 10000000 -ns 100000
+# python3 ../scripts/bit_exploration.py -e "fixed_pt" -n 10000000 -ns 100000
+# python3 ../scripts/bit_exploration.py -e "data" -n 10000000 -ns 100000
