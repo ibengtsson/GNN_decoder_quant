3ba6a0f5a196aed9ebc983aa54a8925b4abb0b34
Isak Bengtsson <isakb97@gmail.com>
2024-02-23 00:48:01 +0100

    run on gpu

--- bash_scripts/belief_matching.sh
+++ bash_scripts/belief_matching.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #SBATCH -A NAISS2023-5-353 -p alvis
-#SBATCH -t 7-00:00:00 			# time limit days-hours:minutes:seconds
-#SBATCH -J update_paper_training
-#SBATCH -o ../job_outputs/train_for_paper%j.out
+#SBATCH -t 3-00:00:00 			# time limit days-hours:minutes:seconds
+#SBATCH -J belief_matching
+#SBATCH -o ../job_outputs/belief_matching_%j.out
 #SBATCH --cpus-per-task=1
 #SBATCH --gpus-per-node=A40:1 # GPUs 64GB of RAM; cost factor 1.0
 
