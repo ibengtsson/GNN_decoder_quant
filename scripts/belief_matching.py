4f468ce71bce3bfecbdf85b7baae09bd25078f42
Isak Bengtsson <isakb97@gmail.com>
2024-02-23 10:44:05 +0100

    divide work in batches

--- scripts/belief_matching.py
+++ scripts/belief_matching.py
@@ -12,22 +12,32 @@ def main():
     # error rate and number of shots
     p = 1e-3
     n = int(1e8)
+    n_per_batch = int(1e6)
+    n_batches = int(n / n_per_batch)
+    
 
     # where to save
-    save_path = Path("../data/belief_matching_p1e-3_d_3_5_7_9_dt_3_4_5_6_7_8_9_10_11")
+    save_path = Path("../data/belief_matching_p1e-3_d_3_5_7_9_dt_3_4_5_6_7_8_9_10_11_batch")
 
     code_sizes = [3, 5, 7, 9]
     rounds = [3, 4, 5, 6, 7, 8, 9, 10, 11]
     data = np.zeros((4, 9))
     for i, d in enumerate(code_sizes):
         for j, r in enumerate(rounds):
-            sim = SurfaceCodeSim(r, d, p, n)
+            sim = SurfaceCodeSim(r, d, p, n_per_batch)
             circuit = sim.circuit
             bm = BeliefMatching(circuit, max_bp_iters=20)
 
-            shots, observables, n_trivial = sim.sample_syndromes()
-            preds = bm.decode_batch(shots)
-            n_correct = np.sum(preds == observables[:, None])
+            n_trivial = 0
+            n_correct = 0
+            for _ in range(n_batches):
+                shots, observables, _n_trivial = sim.sample_syndromes()
+                preds = bm.decode_batch(shots)
+                _n_correct = np.sum(preds == observables[:, None])
+                
+                n_trivial += _n_trivial
+                n_correct += _n_correct
+                
             failure_rate = (n - n_correct - n_trivial) / n
             data[i, j] = failure_rate
 
