1b1545c6b1e4242e72504e8b7a66e0fb772a7aff
Isak Bengtsson <isakb97@gmail.com>
2024-03-07 22:43:06 +0100

    parallel implementation

--- scripts/belief_matching.py
+++ scripts/belief_matching.py
@@ -1,46 +1,88 @@
 import numpy as np
 from beliefmatching import BeliefMatching
+from multiprocessing.pool import Pool
+from multiprocessing import cpu_count
 from pathlib import Path
 import sys
 
 sys.path.append("../")
 from src.simulations import SurfaceCodeSim
 
+def decode(code_sz, reps, n=int(1e3), p=1e-3):
+
+    sim = SurfaceCodeSim(reps, code_sz, p, n)
+    bm = BeliefMatching(sim.circuit, max_bp_iters=20)
+
+    shots, observables, n_trivial = sim.sample_syndromes()
+    preds = bm.decode_batch(shots)
+    n_correct = np.sum(preds == observables[:, None]) 
+
+    return {(code_sz, reps): n_correct}, {(code_sz, reps): n_trivial}
 
 def main():
-
+    
+    # where to save
+    save_path = Path("../data/bm_parallel")
+    
     # error rate and number of shots
     p = 1e-3
-    n = int(1e8)
-    n_per_batch = int(1e6)
+    n = int(1e4)
+    n_per_batch = int(1e3)
     n_batches = int(n / n_per_batch)
     
-
-    # where to save
-    save_path = Path("../data/belief_matching_p1e-3_d_3_5_7_9_dt_3_4_5_6_7_8_9_10_11_batch")
-
-    code_sizes = [3, 5, 7, 9]
+    # initialise sims and decoders
+    code_sizes = [7]
     rounds = [3, 4, 5, 6, 7, 8, 9, 10, 11]
-    data = np.zeros((4, 9))
-    for i, d in enumerate(code_sizes):
-        for j, r in enumerate(rounds):
-            sim = SurfaceCodeSim(r, d, p, n_per_batch)
-            circuit = sim.circuit
-            bm = BeliefMatching(circuit, max_bp_iters=20)
+    
+    # create dictionary labels used later
+    labels = []
+    for sz in code_sizes:
+        for r in rounds:
+            labels.append((sz, r))
+    
+    all_code_sz = np.array([[sz] * len(rounds) for sz in code_sizes]).flatten()
+    all_rounds = np.array(rounds * 2)
+    
+    all_code_sz = np.tile(all_code_sz, n_batches)
+    all_rounds = np.tile(all_rounds, n_batches)
+    p = np.ones_like(all_rounds) * p
+    n_per_batch = np.ones_like(all_rounds) * n_per_batch
+    
+    with Pool(processes=(cpu_count() - 1)) as pool:
+        res = pool.starmap(decode, list(zip(all_code_sz, all_rounds, n_per_batch, p)))
 
-            n_trivial = 0
-            n_correct = 0
-            for _ in range(n_batches):
-                shots, observables, _n_trivial = sim.sample_syndromes()
-                preds = bm.decode_batch(shots)
-                _n_correct = np.sum(preds == observables[:, None])
-                
-                n_trivial += _n_trivial
-                n_correct += _n_correct
-                
-            failure_rate = (n - n_correct - n_trivial) / n
-            data[i, j] = failure_rate
-
+    # collect the statistics
+    correct = {}
+    trivial = {}
+    for c, t in res:
+        c_key = list(c.keys())[0]
+        c_val = list(c.values())[0]
+        t_key = list(t.keys())[0]
+        t_val = list(t.values())[0]
+        
+        if c_key in correct:
+            correct[c_key] += c_val
+        else: 
+            correct.update(c)
+        
+        if t_key in trivial:
+            trivial[t_key] += t_val
+        else:
+            trivial.update(t)
+    
+    data = np.zeros((len(code_sizes), len(rounds)))
+    row_map = {sz: i for i, sz in enumerate(code_sizes)}
+    col_map = {r: i for i, r in enumerate(rounds)}
+    
+    for l in labels:
+        n_correct = correct[l]
+        n_trivial = trivial[l]
+        
+        logical_accuracy = (n - n_correct - n_trivial) / n
+        sz = l[0]
+        rep = l[1]
+        data[row_map[sz], col_map[rep]] = logical_accuracy
+    
     np.save(save_path.parent / (save_path.name + ".npy"), data)
     np.savetxt(
         save_path.parent / (save_path.name + ".csv"),
@@ -48,7 +90,6 @@ def main():
         delimiter=",",
     )
     return 0
-
-
+        
 if __name__ == "__main__":
     main()
